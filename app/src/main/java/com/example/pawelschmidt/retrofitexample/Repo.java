b14277879589ff9da2f776f72e5dd3011ade1754
Pawel Schmidt <px.sxxx@gmail.com>
2017-08-08 19:23:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pawelschmidt/retrofitexample/Repo.java
@@ -0,0 +1,15 @@
+package com.example.pawelschmidt.retrofitexample;
+
+public class Repo {
+
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
